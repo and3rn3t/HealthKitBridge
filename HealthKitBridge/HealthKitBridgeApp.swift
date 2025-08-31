dc0dd41622ada3dee33529f54b0056dfef366ebb
Matthew Anderson <and3rn3t@icloud.com>
2025-08-31 09:46:16 -0500

    Initial Commit

---
+++ HealthKitBridge/HealthKitBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitBridgeApp.swift
+//  HealthKitBridge
+//
+//  Created by Matthew Anderson on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthKitBridgeTests/HealthKitBridgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitBridgeTests.swift
+//  HealthKitBridgeTests
+//
+//  Created by Matthew Anderson on 8/31/25.
+//
+
+import Testing
+@testable import HealthKitBridge
+
+struct HealthKitBridgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
